e3640ecb35fbef93210a705470c68cac1db81221
namlh <hoainamle023@gmail.com>
2022-02-13 22:45:11 +0700

    Hero & Navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import React, {useState, useEffect} from 'react';
 import "./assets/scss/main.scss";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "./assets/images/logo.svg";
 import menu from "./assets/images/icon-hamburger.svg";
+import iconClose from './assets/images/icon-close.svg';
 
 function App() {
+  const [menuIcon, setMenuIcon] = useState(menu);
+
+  function checkMenuIconEqual(value) {
+    return menuIcon === value ? true : false
+  }
   return (
     <div className="main" id="main">
-      <Navbar className="main__nav" expand="xxl" fixed="top">
-        <Container>
+      <Navbar className={`main__nav my-container ${checkMenuIconEqual(iconClose) ? 'my-nav' : null}`} expand="xxl" fixed="top">
+        <Container fluid>
           <Navbar.Brand href="#home">
             <img src={logo} alt="Loopstudios" />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <img src={menu} alt="" aria-hidden="true" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => checkMenuIconEqual(menu) ? setMenuIcon(iconClose) : setMenuIcon(menu)}>
+            <img src={menuIcon} alt="" aria-hidden="true" />
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -25,7 +32,7 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <section className="hero">
+      <section className="hero my-container">
         <div className="hero__content">
           <h1 className="h1-like h1-upper h1-light">
             Immersive experiences that deliver
